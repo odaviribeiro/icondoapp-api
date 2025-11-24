2dc5c4cf83e51358274b49fbba5a629754bb01cd
Davi Ribeiro <46770855+odaviribeiro@users.noreply.github.com>
2025-11-24 18:43:14 -0300

    ♻️ auth - update validation bearer token

--- src/modules/auth/middleware.ts
+++ src/modules/auth/middleware.ts
@@ -1,13 +1,13 @@
 import { HTTPException } from 'hono/http-exception'
 import type { AuthContext } from './types'
-import { verifyJwt } from '../../lib'
+import { verifyAccessToken } from '../../lib/jwt'
 
 export const authMiddleware = async (c: AuthContext, next: () => Promise<void>) => {
-  const header = c.req.header('authorization')
+  const header = c.req.header('authorization') || c.req.header('Authorization')
   if (!header) throw new HTTPException(401, { message: 'No token' })
 
-  const token = header.replace('Bearer ', '')
-  const payload = await verifyJwt(token)
+  const token = header.replace(/^Bearer\s+/i, '')
+  const payload = await verifyAccessToken(token)
 
   if (!payload) throw new HTTPException(401, { message: 'Invalid token' })
 
